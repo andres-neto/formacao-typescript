53aea19bd5e9ac454caf3c337081ccfc074c0650
Andres Neto <andres.fgn@gmail.com>
2024-08-28 16:08:17 -0300

    Ultimas mudancas no codigo nao commitadas

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,3 +5,8 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
 });
+
+// import { Negociacao } from './models/negociacao.js';
+
+// const negociacao = new Negociacao(new Date(), 10, 1000);
+// console.log(negociacao.volume);
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,14 +1,29 @@
 export class Negociacao {
+    #data;
+    #quantidade;
+    #valor;
+
     constructor(_data, quantidade, valor) {
-        this._data = _data;
-        this.quantidade = quantidade;
-        this.valor = valor;
+        this.#data = _data;
+        this.#quantidade = quantidade;
+        this.#valor = valor;
     }
-    get volume() {
-        return this.quantidade * this.valor;
-    }
+
     get data() {
-        const data = new Date(this._data.getTime());
+        // const data = new Date(this.#data.getTime());
+        const data = this.#data;
         return data;
+    }
+    get quantidade() {
+        const quantidade = this.#quantidade;
+        return quantidade;
+    }
+    get valor() {
+        const valor = this.#valor;
+        return valor;
+    }
+    
+    get volume() {
+        return this.#quantidade * this.#valor;
     }
 }
